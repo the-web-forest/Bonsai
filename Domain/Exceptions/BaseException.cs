f72634cc29c7a5879912ab4de1b43b4c44c8518d
Barros <mdbf42@gmail.com>
2022-08-18 23:29:05 -0300

    Fix @ Exception

--- Domain/Exceptions/BaseException.cs
+++ Domain/Exceptions/BaseException.cs
@@ -1,15 +1,30 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace Bonsai.Domain.Errors
 {
     [Serializable]
     public abstract class BaseException: Exception
     {
-        protected BaseException(string Code, string Message) : base(Message)
+
+        public BaseException()
+        {
+        }
+
+        public BaseException(string Code, string Message) : base(Message)
         {
             Data.Add("Code", "BON-"+Code);
             Data.Add("Message", Message);
             Data.Add("ShortMessage", Message.Replace(" ", string.Empty));
+        }
+
+        public BaseException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BaseException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
         }
 
     }
