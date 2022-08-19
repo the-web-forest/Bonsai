6595c0a9480935e4afc44e7e5acd78b307c3c090
Barros <mdbf42@gmail.com>
2022-08-18 23:50:47 -0300

    Fix @ Exceptions

--- Domain/Exceptions/PaymentDeniedException.cs
+++ Domain/Exceptions/PaymentDeniedException.cs
@@ -1,8 +1,11 @@
-﻿namespace Bonsai.Domain.Errors
+﻿using System.Runtime.Serialization;
+
+namespace Bonsai.Domain.Errors
 {
     [Serializable]
     public class PaymentDeniedException: BaseException
     {
         public PaymentDeniedException(): base("002", "Payment Denied") { }
+        protected PaymentDeniedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
--- Domain/Exceptions/UnauthorizedException.cs
+++ Domain/Exceptions/UnauthorizedException.cs
@@ -1,7 +1,11 @@
-﻿namespace Bonsai.Domain.Errors
+﻿using System.Runtime.Serialization;
+
+namespace Bonsai.Domain.Errors
 {
+    [Serializable]
     public class UnauthorizedException: BaseException
     {
         public UnauthorizedException(): base("001", "Unauthorized") { }
+        protected UnauthorizedException(SerializationInfo info, StreamingContext context) : base(info, context) { }
     }
 }
